1c5069ea74361b90d6991fec8a396d47a6e064c5
Jay T <jaytula@gmail.com>
2021-05-04 16:32:34 -0700

    29. Introducing State

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,15 +1,24 @@
-import { MouseEventHandler } from "react";
+import { Fragment, MouseEventHandler, useState } from "react";
+import Backdrop from "./Backdrop";
+import Modal from "./Modal";
 
-function Todo({text}: {text: string}) {
-  const deleteHandler: MouseEventHandler<HTMLButtonElement> = function(event) {
-  }
+function Todo({ text }: { text: string }) {
+  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
+
+  const deleteHandler: MouseEventHandler<HTMLButtonElement> = function (event) {
+    setModalIsOpen(true);
+  };
 
   return (
     <div className="card">
       <h2>{text}</h2>
       <div className="actions">
-        <button className="btn" onClick={deleteHandler}>Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
+      {modalIsOpen && <Modal />}
+      {modalIsOpen && <Backdrop />}
     </div>
   );
 }
