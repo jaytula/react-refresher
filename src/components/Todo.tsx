ddd557c755c3bf5a44ca479196a007a3d2084083
Jay T <jaytula@gmail.com>
2021-05-04 16:22:49 -0700

    27. Handling Events

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,9 +1,14 @@
+import { MouseEventHandler } from "react";
+
 function Todo({text}: {text: string}) {
+  const deleteHandler: MouseEventHandler<HTMLButtonElement> = function(event) {
+  }
+
   return (
     <div className="card">
       <h2>{text}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>Delete</button>
       </div>
     </div>
   );
