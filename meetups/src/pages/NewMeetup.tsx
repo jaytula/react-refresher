3478283b72ab38f9c6977a12da88b345b033eca5
Jay T <jaytula@gmail.com>
2021-05-05 13:29:49 -0700

    40. Sending a POST Http Request

--- meetups/src/pages/NewMeetup.tsx
+++ meetups/src/pages/NewMeetup.tsx
@@ -1,10 +1,23 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-  function addMeetupHandler(meetupData: {title: string, image: string, address: string, description: string}) {
-
+  function addMeetupHandler(meetupData: {
+    title: string;
+    image: string;
+    address: string;
+    description: string;
+  }) {
+    fetch(`${process.env.REACT_APP_FIREBASE_BACKEND}meetups.json`, {
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      body: JSON.stringify(meetupData),
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    });
   }
-  
+
   return (
     <section>
       <h1>Add New Meetup</h1>
