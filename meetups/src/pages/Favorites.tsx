fbe862c6f897f83fb8969bb4bdb2ee8caf035dc9
Jay T <jaytula@gmail.com>
2021-05-05 14:44:54 -0700

    47. More Context Usage

--- meetups/src/pages/Favorites.tsx
+++ meetups/src/pages/Favorites.tsx
@@ -1,5 +1,23 @@
+import { useContext } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+import FavoritesContext from "../store/favorites-context";
+
 function FavoritesPage() {
-  return <div>Favorites Page</div>;
+  const { favorites, totalFavorites } = useContext(FavoritesContext);
+
+  let content: JSX.Element;
+
+  if (totalFavorites === 0) {
+    content = <p>You got not favorites yet. Start adding some?</p>;
+  } else {
+    content = <MeetupList meetups={favorites} />;
+  }
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
+  );
 }
 
 export default FavoritesPage;
