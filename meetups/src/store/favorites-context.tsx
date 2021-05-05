a6a98e197e4f0a8f56b54688e5382ceb88f58110
Jay T <jaytula@gmail.com>
2021-05-05 14:21:48 -0700

    45. Updating State Based On Previous State

--- meetups/src/store/favorites-context.tsx
+++ meetups/src/store/favorites-context.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useState } from "react";
 import { IMeetup } from "../components/meetups/MeetupItem";
+import MeetupList from "../components/meetups/MeetupList";
 
 const FavoritesContext = createContext<{
   favorites: IMeetup[];
@@ -15,6 +16,20 @@ export function FavoritesContextProvider({
   children: ReactNode;
 }) {
   const [userFavorites, setUserFavorites] = useState<IMeetup[]>([]);
+
+  function addFavoriteHandler(favoriteMeetup: IMeetup) {
+    setUserFavorites((meetups) => meetups.concat(favoriteMeetup));
+  }
+
+  function removeFavoriteHandler(meetupId: string) {
+    setUserFavorites((meetups) =>
+      meetups.filter((meetup) => meetup.id !== meetupId)
+    );
+  }
+
+  function itemIsFavoriteHandler(meetupId: string) {
+    return userFavorites.some(meetup => meetup.id === meetupId)
+  }
 
   const context = {
     favorites: userFavorites,
